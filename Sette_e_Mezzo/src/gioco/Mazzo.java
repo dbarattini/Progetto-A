70697575c99e3c66ec0e7241b94ebe35ecb572b8
cl418308 <cl418308@aga41>
2017-04-11 15:33:25 +0200

    mazzo

---
+++ Sette_e_Mezzo/src/gioco/Mazzo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gioco;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author cl418308
+ */
+public class Mazzo {
+    private ArrayList<Carta> da_giocare= new ArrayList<Carta>();
+    private ArrayList<Carta> giocate= new ArrayList<Carta>();
+}
