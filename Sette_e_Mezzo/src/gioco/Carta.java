88af8499468a64262ef230365a4da0f2169fb2a6
cl418308 <cl418308@aga41>
2017-04-11 15:32:52 +0200

    carta

---
+++ Sette_e_Mezzo/src/gioco/Carta.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gioco;
+
+import costanti.Seme;
+import costanti.Valore_carta;
+
+/**
+ *
+ * @author cl418308
+ */
+public class Carta {
+    private Valore_carta valore;
+    private Seme seme;
+    
+}
