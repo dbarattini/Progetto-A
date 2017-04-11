b794e8620aab13b27c5294f519181a3e9e29fd6a
cl418308 <cl418308@aga41>
2017-04-11 15:33:08 +0200

    giocatore

---
+++ Sette_e_Mezzo/src/gioco/Giocatore.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gioco;
+
+/**
+ *
+ * @author cl418308
+ */
+public class Giocatore {
+    private String nome;
+    private int posizione;
+    private boolean mazziere;
+    private boolean scoperta;
+}
