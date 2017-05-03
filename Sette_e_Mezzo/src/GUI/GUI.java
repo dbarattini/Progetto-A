d7ea1443747c10afd1acb8deb529a9903de716d0
Martino <Martino@Martino-PC>
2017-05-03 19:10:20 +0200

    Prove JPanel e posizionamento di questo superate!!!!

--- Sette_e_Mezzo/src/GUI/GUI.java
+++ Sette_e_Mezzo/src/GUI/GUI.java
@@ -1,25 +1,39 @@
 package GUI;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.net.URL;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GUI extends JFrame {
+    
+    PannelloImmagine pannello;
     
     public GUI(String nome) {
         setTitle(nome);
         setPreferredSize(new Dimension(1280, 720));
 	setMinimumSize(new Dimension(1280, 720));		
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	setResizable(false);
 	setLocationRelativeTo(null);
+        this.getContentPane().setLayout(null);        
+        
+        pannello = new PannelloImmagine(caricaImmagine("immagini/AssoDenari.png"), new BorderLayout());
+        pannello.posiziona(55, 500);
+        add(pannello);
+        
         setVisible(true);
     }
     
     public ImageIcon caricaImmagine(String nome){
 	ClassLoader caricatore = getClass().getClassLoader();
-	URL percorso = caricatore.getResource("/immagini/" + nome);
+	URL percorso = caricatore.getResource(nome);
 	return new ImageIcon(percorso);
     }
+    
+    
 }
--- Sette_e_Mezzo/src/GUI/ProvaGUI.java
+++ Sette_e_Mezzo/src/GUI/ProvaGUI.java
@@ -2,4 +2,7 @@ package GUI;
 
 public class ProvaGUI {
     
+    public static void main(String[] args) {
+        GUI gui = new GUI("Sette e Mezzo");
+    }
 }
