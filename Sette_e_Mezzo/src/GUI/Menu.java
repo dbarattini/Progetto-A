f752eda0f59f12a4dd077539e552e05b0faf9d00
Martino <Martino@Martino-PC>
2017-05-04 15:49:42 +0200

    Aggiunti i listener dei bottoni, creato messaggio per le regole ( che sono però da scrivere )

--- Sette_e_Mezzo/src/GUI/Menu.java
+++ Sette_e_Mezzo/src/GUI/Menu.java
@@ -1,17 +1,24 @@
 package GUI;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Menu extends JFrame {
     
-    private JButton partita_off, partita_on, regole, opzioni;
+    private JButton partita_off, partita_on, regole, opzioni, indietro;
     private Sfondo sfondo;
+    private JLabel regole_scritte;
     
     public Menu() {
         setTitle("Menu");
@@ -23,6 +30,12 @@ public class Menu extends JFrame {
 	setLocationRelativeTo(null);
         getContentPane().setLayout(null);
         
+        inizializza_GUI();
+        
+        setVisible(true);
+    }
+    
+    private void inizializza_GUI() {
         sfondo = new Sfondo("immagini/sfondomenu.jpg", 800, 570);
         sfondo.setBounds(0, 0, 800, 600);
         add(sfondo);
@@ -31,18 +44,56 @@ public class Menu extends JFrame {
         partita_on = new JButton(caricaImmagine("immagini/online.png"));
         regole = new JButton(caricaImmagine("immagini/regole.png"));
         opzioni = new JButton(caricaImmagine("immagini/opzioni.png"));
+        indietro = new JButton(caricaImmagine("immagini/indietro.png"));
+        regole_scritte = new JLabel(caricaImmagine("immagini/AssoDenari.png"));
         
         partita_off.setBounds(this.getWidth()/2 - 100, 150, 200, 80);
         partita_on.setBounds(this.getWidth()/2 - 100, 250, 200, 80);
         regole.setBounds(this.getWidth()/2 - 100, 350, 200, 80);
         opzioni.setBounds(this.getWidth()/2 - 100, 450, 200, 80);
+        indietro.setBounds(this.getWidth()/2 - 100, 470, 200, 80);
+        regole_scritte.setBounds(this.getWidth()/2 - 50, 150, 500, 50);
+        
+        partita_off.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // codice per gioca offline
+            };
+        });
+        
+        partita_on.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // codice per gioca online
+            };
+        });
+        
+        regole.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Regole di gioco:\nScrivere le regole di gioco",
+                                            "REGOLE DI GIOCO", JOptionPane.INFORMATION_MESSAGE);
+            };
+        });
+        
+        opzioni.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // codice per opzioni
+            };
+        });
+        
+        indietro.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // codice per indietro
+            };
+        });
         
         sfondo.add(partita_off);
         sfondo.add(partita_on);
         sfondo.add(regole);
         sfondo.add(opzioni);
-        
-        setVisible(true);
     }
     
     private ImageIcon caricaImmagine(String nome){
--- Sette_e_Mezzo/src/GUI/ProvaGUI.java
+++ Sette_e_Mezzo/src/GUI/ProvaGUI.java
@@ -3,7 +3,7 @@ package GUI;
 public class ProvaGUI {
     
     public static void main(String[] args) {
-        GUI gui = new GUI("Sette e Mezzo");
+        //GUI gui = new GUI("Sette e Mezzo");
         Menu menu = new Menu();
     }
 }
