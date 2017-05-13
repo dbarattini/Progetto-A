19da9f98ecddba18fa69ba1a377b026e610ba4e0
Max & family <Max & family@Lenovo>
2017-05-13 17:38:43 +0200

    Aggiunto enum ImpostazioniMusica

---
+++ Sette_e_Mezzo/src/classi_dati/ImpostazioniMusica.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classi_dati;
+
+/**
+ *
+ * @author Max & family
+ */
+public enum ImpostazioniMusica {
+    
+    Play, Stop, Riavvia, Loop, Carica
+}
