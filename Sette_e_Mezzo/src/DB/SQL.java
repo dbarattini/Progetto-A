8f4fff585c86d00c730eb90cebfb9a15897b9b2e
marco <marco@Marco>
2017-05-13 16:20:41 +0200

    database settato

--- Sette_e_Mezzo/src/DB/SQL.java
+++ Sette_e_Mezzo/src/DB/SQL.java
@@ -86,37 +86,54 @@ public class SQL {
     System.out.println("fiches aggiornate con successo");
   }
     
-     public  void getFiches( String user )
+   public int getFiches( String user )
   {
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:setteEmezzo.db");
-            c.setAutoCommit(false);
-      S
-
-      stmt = c.createStatement();
-      ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
-      while ( rs.next() ) {
-         int id = rs.getInt("id");
-         String  name = rs.getString("name");
-         int age  = rs.getInt("age");
-         String  address = rs.getString("address");
-         float salary = rs.getFloat("salary");
-         System.out.println( "ID = " + id );
-         System.out.println( "NAME = " + name );
-         System.out.println( "AGE = " + age );
-         System.out.println( "ADDRESS = " + address );
-         System.out.println( "SALARY = " + salary );
-         System.out.println();
-      }
-      rs.close();
-      stmt.close();
-      c.close();
-    } catch ( Exception e ) {
-      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-      System.exit(0);
+            c.setAutoCommit(false);   
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM PROFILO;" );
+            while ( rs.next() ) {
+                String  username = rs.getString("USERNAME");
+                int fiches  = rs.getInt("FICHES");
+                if(username.equals(user))
+                    return fiches;
+            }
+            rs.close();
+            chiudiDatabase();
+            } catch ( Exception e ) {
+                  System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                  chiudiDatabase();
+            }
+            return 0;
     }
-    System.out.println("Operation done successfully");
+   
+    public void aggiungiVittoria( String user ){
+            int vittorie=0;    
+            try {
+                    Class.forName("org.sqlite.JDBC");
+                    c = DriverManager.getConnection("jdbc:sqlite:setteEmezzo.db");
+                    c.setAutoCommit(false);
+
+                    ResultSet rs = stmt.executeQuery( "SELECT * FROM PROFILO;" );
+                    while ( rs.next() ) {
+                        String  username = rs.getString("USERNAME");
+                        int vit  = rs.getInt("VITTORIE");
+                        vit++;
+                        if(username.equals(user))
+                            vittorie=vit;
+                    }
+            String dato= "UPDATE PROFILO set VITTORE ="+vittorie+" where USERNAME='"+user+"';";
+            String sql = dato;
+            stmt.executeUpdate(sql);
+            c.commit();
+            chiudiDatabase();
+    } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            chiudiDatabase();
+    }
+    System.out.println("aggiunta vittoria");
   }
     
     private void chiudiDatabase() {
