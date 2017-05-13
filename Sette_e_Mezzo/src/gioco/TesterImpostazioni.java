8f11148e4de2387644b95dc0a4a4ce5584f14560
Max & family <Max & family@Lenovo>
2017-05-13 17:41:40 +0200

    Aggiunto tester Impostazioni

---
+++ Sette_e_Mezzo/src/gioco/TesterImpostazioni.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gioco;
+
+/**
+ *
+ * @author Max & family1
+ * 
+ */
+public class TesterImpostazioni {
+    
+    public static void main (String[] args) throws InterruptedException {
+        
+        Impostazioni impostazioni = new Impostazioni();
+        impostazioni.selezionaImpostazione();
+    }
+}
