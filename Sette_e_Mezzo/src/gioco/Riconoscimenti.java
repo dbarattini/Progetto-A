8e104a667ff818d63bc7a18d3076f885a5b40bec
Max & family <Max & family@Lenovo>
2017-05-14 17:51:36 +0200

    Aggiunta classe Riconoscimenti

---
+++ Sette_e_Mezzo/src/gioco/Riconoscimenti.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gioco;
+
+/**
+ *
+ * @author Max & family
+ */
+public class Riconoscimenti {
+
+    public Riconoscimenti() {
+    }
+    
+    public void printRiconoscimenti() {
+        System.out.println("SETTE E MEZZO:\nDEVELOPERS:\n Daneiele Barattini, Marco Prina, Marco Guido, Martino Bergamaschi, Samba Mbaye, Samuele Colmi\n");
+    }
+}
