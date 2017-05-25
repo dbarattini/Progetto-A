81e72eaa1cef114ba9759b05398505a4051c6467
samba <samba@localhost>
2017-05-25 15:58:22 +0200

    abbozzo GUI di Regole di gioco.

--- Sette_e_Mezzo/src/regole/view/RegoleGuiView.java
+++ Sette_e_Mezzo/src/regole/view/RegoleGuiView.java
@@ -1,10 +1,36 @@
 package regole.view;
 
-import javax.swing.JOptionPane;
+import static com.sun.glass.ui.Cursor.setVisible;
+import java.awt.BorderLayout;
+import java.awt.event.WindowListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import regole.model.RegoleModel;
 
-public class RegoleGuiView extends JOptionPane{
+public class RegoleGuiView{
+    
     public RegoleGuiView(RegoleModel model){
-        this.showMessageDialog(null, model.getRegole(), "REGOLE DI GIOCO", JOptionPane.INFORMATION_MESSAGE);
+        JFrame finestra = new JFrame();
+        finestra.setSize(700, 500);
+        WindowListener chiudi = null;
+        finestra.addWindowListener(chiudi);
+        finestra.setTitle("REGOLE DI GIOCO");
+        
+        //BorderLayout gestore = new BorderLayout();
+        //setLayout(gestore);
+        JPanel pannelloRegole = new JPanel();
+        
+        
+        /*Container contenuto = getContentPane();
+        contenuto.setBackground(Color.CYAN);
+        */
+        JTextArea stampaRegole = new JTextArea(model.getRegole());
+        
+        finestra.add(stampaRegole, BorderLayout.CENTER);
+        
+        finestra.setVisible(true);
     }
+
+   
 }
