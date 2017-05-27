b2b12d8aa90f6cc9739d952690115e1f464411cc
marco <>
2017-05-27 10:14:35 +0200

    aggiunto leggi

---
+++ Sette_e_Mezzo_Server/src/comunicazione/Leggi.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comunicazione;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Observable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author root
+ */
+public class Leggi extends Observable implements Runnable{
+private BufferedReader reader;
+    private String message;
+    private boolean running = true;
+    PrintStream out;
+        
+    public Leggi(BufferedReader in, PrintStream out) {
+        reader = in;
+        this.out = out;
+    }
+    
+    public void run() {
+        while(running) {
+            try {
+                message = reader.readLine();
+                printMessage();
+            } catch (IOException ex) {
+                Logger.getLogger(Leggi.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public void printMessage() {
+        if(message != null){
+            out.println(message);
+            this.setChanged();
+            this.notifyObservers(message);
+        }
+    }
+    
+}
