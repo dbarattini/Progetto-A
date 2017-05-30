c8c47788b47f0ff5924d17e711a1cdb6a6aaab41
Max & family <Max & family@Lenovo>
2017-05-30 11:22:27 +0200

    Modifica ProvaPartitaOffline

--- Sette_e_Mezzo/src/gioco/ProvaPartitaOffline.java
+++ Sette_e_Mezzo/src/gioco/ProvaPartitaOffline.java
@@ -1,11 +1,20 @@
 package gioco;
 
 import classi_dati.DifficoltaBot;
+import eccezioni.datoGiaPresente;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ProvaPartitaOffline {
-    public static void main(String[] args) throws InterruptedException {
-        PartitaOffline partita = new PartitaOffline(2, 100, DifficoltaBot.Facile, System.in, System.out, System.err);
-        partita.gioca();
+    public static void main(String[] args) {
+        PartitaOffline partita;
+        try {
+            partita = new PartitaOffline(2, 100, DifficoltaBot.Facile, System.in, System.out, System.err);
+            partita.gioca();
+        } catch (InterruptedException | datoGiaPresente ex) {
+            Logger.getLogger(ProvaPartitaOffline.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 }
