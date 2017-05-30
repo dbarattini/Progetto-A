c634866ce0b50020d5166309912386c989b5304e
Max & family <Max & family@Lenovo>
2017-05-30 11:20:16 +0200

    Creata Classe Profilo

--- Sette_e_Mezzo/src/gioco/Profilo.java
+++ Sette_e_Mezzo/src/gioco/Profilo.java
@@ -6,7 +6,11 @@
 package gioco;
 
 import DB.SQL;
-import giocatori.Giocatore;
+import eccezioni.NameNotFoundException;
+import giocatori.GiocatoreUmano;
+import static java.lang.System.in;
+import static java.lang.System.out;
+import java.util.Scanner;
 
 /**
  *
@@ -14,8 +18,39 @@ import giocatori.Giocatore;
  */
 public class Profilo {
     
+    private GiocatoreUmano giocatore;
     private SQL db;
     
+
+    public Profilo() {
+         this.db = new SQL();
+    }
+
+    public void selezionaProfilo() {
+        
+        while(true) {
+            printOpzioniProfilo();
+            scegliProfilo();
+        }
+    }
     
+    private void printOpzioniProfilo() {
+        out.println("\n");
+        out.println("Inserire nome dell'utente per visualizzare il profilo:\n");
+    }
     
-}
+    private void scegliProfilo() {
+        Scanner scanner = new Scanner(in);        
+        String input = scanner.next();
+        
+        if (db.esisteNome(input)) {
+            System.out.println("PROFILO:\n");
+            System.out.println("  -NOME: " +input);
+            System.out.println("  -FICHES: " +db.getFiches(input));
+            System.out.println("  -VITTORIE: " +db.getVittorie(input));
+        } else
+            System.out.println("Profilo non esistente\n");
+    }
+    
+}    
+    
