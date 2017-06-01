07d78c9bf5c246a1457f5116099874cc43576c01
samba <samba@localhost>
2017-06-01 15:27:53 +0200

    abbozzo RiconoscimentiGui

--- Sette_e_Mezzo/src/GUI/RiconoscimentiGui.java
+++ Sette_e_Mezzo/src/GUI/RiconoscimentiGui.java
@@ -1,11 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package GUI;
 
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import regole.view.ChiudiRegole;
 
 /**
  *
@@ -13,7 +13,25 @@ import javax.swing.JFrame;
  */
 public class RiconoscimentiGui extends JFrame{
     
-    
-    }
-    
+  public RiconoscimentiGui(){
+        setSize(500, 300);
+        addWindowListener(new ChiudiRiconoscimenti());
+        setTitle("RICONOSCIMENTI");
+        
+        setLayout(new BorderLayout());
+        JPanel pannello = new JPanel();
+        JTextArea riconoscimenti = new JTextArea ("Developed by:\n"
+                            + "Barattini Daniel\n" 
+                            + "Bergamaschi Martino\n" 
+                            + "Colmi Samuele\n"
+                            + "Guido Marco\n" 
+                            + "Mbaye Samba\n" 
+                            + "Prina Marco\n");
+        riconoscimenti.setEditable(false);
+        add(riconoscimenti, BorderLayout.CENTER);
+        
+        
+        setVisible(true);
+  }
+   
 }
