f9fc1dc85aa8fa4c1dc9e6cb6d64f29e505c668e
samba <samba@localhost>
2017-06-01 14:31:00 +0200

    creata classe RiconoscimentiGui

---
+++ Sette_e_Mezzo/src/GUI/RiconoscimentiGui.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author samba
+ */
+public class RiconoscimentiGui extends JFrame{
+    
+    
+    }
+    
+}
