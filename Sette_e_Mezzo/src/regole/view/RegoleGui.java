e392d624aed23f5e185f504178b90c9946d0df7c
samba <samba@localhost>
2017-06-01 14:22:39 +0200

    finestra messa non Resizable e spostato pulsante INDIETRO.

--- Sette_e_Mezzo/src/regole/view/RegoleGui.java
+++ Sette_e_Mezzo/src/regole/view/RegoleGui.java
@@ -1,6 +1,9 @@
 package regole.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -12,15 +15,16 @@ public class RegoleGui extends JFrame{
     
     public RegoleGui(){
         
-        setSize(1000, 300);
+        setSize(800, 400);
         addWindowListener(new ChiudiRegole());
         setTitle("REGOLE DI GIOCO");
+        setResizable(false);
         
         setLayout(new BorderLayout());
         
         JPanel pannello = new JPanel();
-        JTextArea regole  = new JTextArea("REGOLE DI GIOCO:\n"
-                        + "\n"
+        pannello.setBackground(Color.WHITE);
+        JTextArea regole  = new JTextArea("\n"
                         + "All'inizio del gioco ogni giocatore riceve una carta, non visibile agli altri giocatori.\n"
                         + "Successivamente si sceglie la puntata del round, che va da un minimo di 1 a un massimo di tutto ciò che si possiede.\n"
                         + "Durante il proprio turno, si può scegliere di ricevere carte o rimanere con la/le carta/e già in possesso.\n"
@@ -32,9 +36,14 @@ public class RegoleGui extends JFrame{
                         + "assume il valore (intero da 1 a 7, o 1/2) desiderato dal giocatore,\n"
                         + "mentre in caso di sette e mezzo reale, si verrà pagati il doppio della posta e, al turno successivo,\n"
                         + "si diverrà mazzieri.\n"
-                        + "Buon sette e mezzo!");    
+                        + "\n"
+                        + "BUON SETTE E MEZZO!");    
         regole.setEditable(false);
         add(regole, BorderLayout.CENTER);
+        
+        JPanel pulsanteIndietro = new JPanel();
+        pulsanteIndietro.setBackground(Color.WHITE);
+        pulsanteIndietro.setLayout(new FlowLayout());
         
         JButton indietro = new JButton("INDIETRO");
         indietro.addActionListener(new ActionListener(){
@@ -42,9 +51,9 @@ public class RegoleGui extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 dispose();
             }
-            
         });
-        add(indietro, BorderLayout.SOUTH);
+        pulsanteIndietro.add(indietro);
+        add(pulsanteIndietro, BorderLayout.SOUTH);
         
         setVisible(true);
     }
