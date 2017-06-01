47fc853d0a4b7ace55db519158cd3aec2b17021d
samba <samba@localhost>
2017-06-01 15:23:21 +0200

    creato classe ProvaGuiRegole per testare RegoleGui

---
+++ Sette_e_Mezzo/src/sette_e_mezzo/ProvaGuiRegole.java
@@ -0,0 +1,16 @@
+
+package sette_e_mezzo;
+
+import GUI.RegoleGui;
+
+/**
+ *
+ * @author samba
+ */
+public class ProvaGuiRegole {
+
+    public static void main(String[] args) {
+      
+        RegoleGui regolegioco = new RegoleGui();
+    }
+}
