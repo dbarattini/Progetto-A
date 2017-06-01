702bba92a9e3901d620e1ebcb5b3b34d59f2cd35
samba <samba@localhost>
2017-06-01 13:28:17 +0200

    creato classe ChiudiRegole per chiudere la finestra

---
+++ Sette_e_Mezzo/src/regole/view/ChiudiRegole.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regole.view;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+/**
+ *
+ * @author samba
+ */
+public class ChiudiRegole extends WindowAdapter {
+
+    public void windowClosing(WindowEvent e) {
+         System.exit(0);
+    }
+
+}
