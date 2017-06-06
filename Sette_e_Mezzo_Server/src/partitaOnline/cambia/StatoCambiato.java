71d172ec95b2f254fa132efdcddedbc5867db6f8
marco <root@kali>
2017-06-06 20:23:23 +0200

    cambiato nome per chiarezza

---
+++ Sette_e_Mezzo_Server/src/partitaOnline/cambia/StatoCambiato.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partitaOnline.cambia;
+
+import dominio.classi_dati.Stato;
+
+/**
+ *
+ * @author root
+ */
+public class StatoCambiato {
+    private String nome;
+    private Stato stato;
+
+    public StatoCambiato(String nome, Stato stato) {
+        this.nome = nome;
+        this.stato = stato;
+    }
+
+    @Override
+    public String toString() {
+        return "cambia\tStato\t" + nome + " " + stato;
+    }
+    
+    
+}
