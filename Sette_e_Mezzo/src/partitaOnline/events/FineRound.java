b1db9eed2b30f858fd1bce4cb958f2f840989a01
marco <root@kali>
2017-06-06 12:06:03 +0200

    tolto giocatore che non poteva essre mandato

--- Sette_e_Mezzo/src/partitaOnline/events/FineRound.java
+++ Sette_e_Mezzo/src/partitaOnline/events/FineRound.java
@@ -3,6 +3,7 @@ package partitaOnline.events;
 import dominio.elementi_di_gioco.Carta;
 import dominio.giocatori.Giocatore;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  *
@@ -10,30 +11,36 @@ import java.io.Serializable;
  */
 public class FineRound implements Serializable {
 
-    private Giocatore giocatore;
+    private String nome;
+    private Carta cartaCoperta;
+    private ArrayList<Carta> carteScoperte;
+    private int fiches, puntata;
+    private boolean isMazziere;
+    
 
-    public FineRound(Giocatore giocatore) {
-        this.giocatore = giocatore;
-    }
-
-    public Giocatore getGiocatore() {
-        return giocatore;
+    public FineRound(String nome, Carta cartaCoperta,  ArrayList<Carta> carteScoperte, int fiches, boolean isMazziere, int puntata) {
+        this.nome=nome;
+        this.cartaCoperta=cartaCoperta;
+        this.carteScoperte=carteScoperte;
+        this.fiches=fiches;
+        this.isMazziere=isMazziere;
+        this.puntata=puntata;
     }
 
     /**
      *
      * @return "evento FineRound " + username + " " +
      * CarteSeparateDaSpazi + " fineCarte " + " " +
-     * "fiches" + "puntata(seNonMazziere)" ;
+     * "fiches" + isMazziere + "puntata(seNonMazziere)" ;
      */
     @Override
     public String toString() {
-        String ritorno = "evento\tFineRound\t" + giocatore.getNome() + " " + giocatore.getCartaCoperta() + " ";
-        for (Carta carta : giocatore.getCarteScoperte()) {
+        String ritorno = "evento\tFineRound\t" + nome+ " " + cartaCoperta + " ";
+        for (Carta carta : carteScoperte) {
             ritorno += carta.toString() + " ";
         }
-        ritorno += "fineCarte "+giocatore.getFiches();
-        if(!giocatore.isMazziere()) ritorno+=" "+giocatore.getPuntata();
+        ritorno += "fineCarte "+fiches+" " + isMazziere;
+        if(!isMazziere) ritorno+=" "+puntata;
         return ritorno;
     }
 
