f5fd8312bdd967a268a8b52bce6c915c8f411a6d
marco <root@kali>
2017-06-07 17:23:04 +0200

    aggiustato booleano

--- Sette_e_Mezzo/src/partitaOnline/controller/PartitaOnlineController.java
+++ Sette_e_Mezzo/src/partitaOnline/controller/PartitaOnlineController.java
@@ -19,20 +19,18 @@ import java.util.logging.Logger;
 import partitaOnline.events.*;
 import partitaOnline.view.PartitaOnlineConsoleView;
 
-
-public class PartitaOnlineController extends Observable implements ViewEventListener, Observer{
+public class PartitaOnlineController extends Observable implements ViewEventListener, Observer {
 
     private Leggi leggi;
-    private ArrayList<GiocatoreOnline> giocatori=new ArrayList<>();
+    private ArrayList<GiocatoreOnline> giocatori = new ArrayList<>();
     private PrintWriter aServer;
     private String nomeLocale;
 
-    public PartitaOnlineController( Socket socket, BufferedReader in) {
+    public PartitaOnlineController(Socket socket, BufferedReader in) {
         try {
-            
 
             //view.addPartitaOfflineViewEventListener(this);
-            this.leggi=new Leggi(in);
+            this.leggi = new Leggi(in);
             this.aServer = new PrintWriter(socket.getOutputStream(), true);
             leggi.addObserver(this);
             Thread t = new Thread(leggi);
@@ -41,53 +39,49 @@ public class PartitaOnlineController extends Observable implements ViewEventList
             Logger.getLogger(PartitaOnlineController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-   
 
     @Override
     public void ViewEventReceived(ViewEvent evt) {
-        if(evt.getArg() instanceof SetPuntata){
-            aServer.println(((SetPuntata)evt.getArg()).toString());
-        } else if(evt.getArg() instanceof SetGiocata){
-            aServer.println(((SetGiocata)evt.getArg()).toString());;
+        if (evt.getArg() instanceof SetPuntata) {
+            aServer.println(((SetPuntata) evt.getArg()).toString());
+        } else if (evt.getArg() instanceof SetGiocata) {
+            aServer.println(((SetGiocata) evt.getArg()).toString());;
         }
-    }   
-    
-    public void riceviEventoDaVista(Object oggetto){
-        if(oggetto instanceof SetPuntata){
-            aServer.println(((SetPuntata)oggetto).toString());
-        } else if(oggetto instanceof SetGiocata){
-            aServer.println(((SetGiocata)oggetto).toString());;
+    }
+
+    public void riceviEventoDaVista(Object oggetto) {
+        if (oggetto instanceof SetPuntata) {
+            aServer.println(((SetPuntata) oggetto).toString());
+        } else if (oggetto instanceof SetGiocata) {
+            aServer.println(((SetGiocata) oggetto).toString());;
         }
     }
 
     @Override
     public void update(Observable o, Object arg) {
-        String messaggio=arg.toString();
-        String dati[]=messaggio.split("\t");
-        
-        if(dati[0].equals("evento")){
+        String messaggio = arg.toString();
+        String dati[] = messaggio.split("\t");
+
+        if (dati[0].equals("evento")) {
             gestisciEvento(dati);
-        }
-        else if(dati[0].equals("setta")){
+        } else if (dati[0].equals("setta")) {
             gestisciSettaggio(dati);
-        }
-        else if(dati[0].equals("cambia")){
+        } else if (dati[0].equals("cambia")) {
             gestisciCambiamento(dati);
         }
 
     }
-    
+
     private void gestisciCambiamento(String[] dati) {
-        switch (dati[1]){
+        switch (dati[1]) {
             case "NuovoGiocatore":
-                String componenti[]=dati[2].split(" ");
+                String componenti[] = dati[2].split(" ");
                 giocatori.add(new GiocatoreOnline(componenti[0], Integer.valueOf(componenti[1])));
                 break;
             case "CartaCoperta":
-                componenti=dati[2].split(" ");
-                String valore= componenti[1].substring(0, 1);
-                String seme= componenti[1].substring(1, 2);
+                componenti = dati[2].split(" ");
+                String valore = componenti[1].substring(0, 1);
+                String seme = componenti[1].substring(1, 2);
                 giocatoreDaNome(componenti[0]).setCartaCoperta(new Carta(valore, seme));
                 break;
             case "GiocatoreDisconnesso":
@@ -97,62 +91,63 @@ public class PartitaOnlineController extends Observable implements ViewEventList
                 setMazziere(dati[2]);
                 break;
             case "StatoCambiato":
-                componenti=dati[2].split(" ");
+                componenti = dati[2].split(" ");
                 giocatoreDaNome(componenti[0]).setStato(Stato.valueOf(componenti[1]));
                 break;
             case "UltimaCartaOttenuta":
-                componenti=dati[2].split(" ");
+                componenti = dati[2].split(" ");
                 giocatoreDaNome(componenti[0]).setUltimaCartaOttenuta(new Carta(componenti[1].substring(0, 1), componenti[1].substring(1, 2)));
                 break;
             case "ValoreMAno":
-                componenti=dati[2].split(" ");
+                componenti = dati[2].split(" ");
                 giocatoreDaNome(componenti[0]).setValoreMano(Double.valueOf(componenti[1]));
                 break;
         }
     }
 
     private void gestisciSettaggio(String[] dati) {
-        if(dati[1].equals("Nome"))
-            this.nomeLocale=dati[2];
+        if (dati[1].equals("Nome")) {
+            this.nomeLocale = dati[2];
+        }
     }
 
     private void gestisciEvento(String[] dati) throws NumberFormatException {
         Object ritorno = null;
-        switch (dati[1]){
+        switch (dati[1]) {
             case "Error":
-                ritorno=new Error(dati[2]);
+                ritorno = new Error(dati[2]);
                 break;
             case "EstrattoMazziere":
-                ritorno= new EstrattoMazziere();
+                ritorno = new EstrattoMazziere();
                 break;
             case "MazzoRimescolato":
-                ritorno= new MazzoRimescolato();
+                ritorno = new MazzoRimescolato();
                 break;
             case "RisultatoManoParticolare":
-                ritorno= new RisultatoManoParticolare();
+                ritorno = new RisultatoManoParticolare();
                 break;
             case "FineManoAvversario":
-                ritorno=fineManoAvversario(dati);
+                ritorno = fineManoAvversario(dati);
                 break;
             case "FineRound":
-                ritorno=fineRound(dati);
+                ritorno = fineRound(dati);
                 break;
             case "MazzierePerde":
-                ritorno= new MazzierePerde();
+                ritorno = new MazzierePerde();
                 break;
             case "AggiornamentoMazziere":
-                ritorno= new AggiornamentoMazziere();
+                ritorno = new AggiornamentoMazziere();
                 break;
             case "GameOver":
-                ritorno= new GameOver();
+                ritorno = new GameOver();
                 break;
             case "RichiediGiocata":
                 ritorno = richediGiocata(dati);
                 break;
             case "RichiediPuntata":
-                ritorno= richiediPunatata(dati);
+                ritorno = richiediPunatata(dati);
                 break;
-                
+
         }
         this.setChanged();
         this.notifyObservers(ritorno);
@@ -160,81 +155,88 @@ public class PartitaOnlineController extends Observable implements ViewEventList
 
     private Object richiediPunatata(String[] dati) throws NumberFormatException {
         Object ritorno;
-        String componenti[]=dati[2].split(" ");
-        Carta coperta = new Carta(componenti[0].substring(0, 1),componenti[0].substring(1, 2));
-        Double valoreMano=Double.valueOf(componenti[1]);
-        int fiches= Integer.valueOf(componenti[2]);
-        ritorno= new RichiediPuntata(coperta, valoreMano, fiches);
+        String componenti[] = dati[2].split(" ");
+        Carta coperta = new Carta(componenti[0].substring(0, 1), componenti[0].substring(1, 2));
+        Double valoreMano = Double.valueOf(componenti[1]);
+        int fiches = Integer.valueOf(componenti[2]);
+        ritorno = new RichiediPuntata(coperta, valoreMano, fiches);
         return ritorno;
     }
 
     private Object richediGiocata(String[] dati) throws NumberFormatException {
         Object ritorno;
-        String componenti[]=dati[2].split(" ");
-        ArrayList<Carta>  carte= new ArrayList();
-        double valoreMano=0;
-        for(int i=0; i<componenti.length; i++){
-            if(componenti[i].equals("fineCarte")){
-                valoreMano=Double.valueOf(componenti[i+1]);
+        String componenti[] = dati[2].split(" ");
+        ArrayList<Carta> carte = new ArrayList();
+        double valoreMano = 0;
+        for (int i = 0; i < componenti.length; i++) {
+            if (componenti[i].equals("fineCarte")) {
+                valoreMano = Double.valueOf(componenti[i + 1]);
                 break;
             }
-            carte.add(new Carta(componenti[i].substring(0, 1),componenti[i].substring(1, 2)));
+            carte.add(new Carta(componenti[i].substring(0, 1), componenti[i].substring(1, 2)));
         }
-        Carta coperta= carte.get(0);
+        Carta coperta = carte.get(0);
         carte.remove(coperta);
-        ritorno= new RichiediGiocata(coperta,carte,valoreMano );
+        ritorno = new RichiediGiocata(coperta, carte, valoreMano);
         return ritorno;
     }
 
     private Object fineRound(String[] dati) throws NumberFormatException {
         Object ritorno;
-        int i=1;
-        String componentiFR[]=dati[2].split(" ");
-        String nome=componentiFR[0];
-        Carta cartaCoperta=new Carta(componentiFR[i].substring(0, 1),componentiFR[i].substring(1, 2));
+        int i = 1;
+        String componentiFR[] = dati[2].split(" ");
+        String nome = componentiFR[0];
+        Carta cartaCoperta = new Carta(componentiFR[i].substring(0, 1), componentiFR[i].substring(1, 2));
         ArrayList<Carta> carteScoperte = new ArrayList();
-        for(i=2; ;i++){
-            if(componentiFR[i].equals("fineCarte"))
+        for (i = 2;; i++) {
+            if (componentiFR[i].equals("fineCarte")) {
                 break;
-            else
-                carteScoperte.add(new Carta(componentiFR[i].substring(0, 1),componentiFR[i].substring(1, 2)));
+            } else {
+                carteScoperte.add(new Carta(componentiFR[i].substring(0, 1), componentiFR[i].substring(1, 2)));
+            }
         }
-        i=i+1;
-        int fiches= Integer.valueOf(componentiFR[i]);
-        i=i+1;
-        boolean isMazziere=Boolean.getBoolean(componentiFR[i]);
-        i=i+1;
-        int puntata=0;
-        if(!isMazziere) puntata=Integer.valueOf(componentiFR[i]);
-        return ritorno= new FineRound(nome,cartaCoperta, carteScoperte, fiches,isMazziere, puntata);
+        i = i + 1;
+        int fiches = Integer.valueOf(componentiFR[i]);
+        i = i + 1;
+        boolean isMazziere = Boolean.valueOf(componentiFR[i]);
+
+        i = i + 1;
+        int puntata = 0;
+        if (!isMazziere) {
+            puntata = Integer.valueOf(componentiFR[i]);
+        }
+        return ritorno = new FineRound(nome, cartaCoperta, carteScoperte, fiches, isMazziere, puntata);
     }
 
     private Object fineManoAvversario(String[] dati) throws NumberFormatException {
         Object ritorno;
-        int i=1;
-        String componenti[]=dati[2].split(" ");
-        String nome=componenti[0];
-        ArrayList<Carta> carteScoperte= new ArrayList();
-        for(i=1; ;i++){
-            if(componenti[i].equals("fineCarte"))
+        int i = 1;
+        String componenti[] = dati[2].split(" ");
+        String nome = componenti[0];
+        ArrayList<Carta> carteScoperte = new ArrayList();
+        for (i = 1;; i++) {
+            if (componenti[i].equals("fineCarte")) {
                 break;
-            else
-                carteScoperte.add(new Carta(componenti[i].substring(0, 1),componenti[i].substring(1, 2)));
+            } else {
+                carteScoperte.add(new Carta(componenti[i].substring(0, 1), componenti[i].substring(1, 2)));
+            }
         }
-        i=i+1;
-        Stato stato=Stato.valueOf(componenti[i]);
-        i=i+1;
-        int puntata= Integer.valueOf(componenti[i]);
-        return ritorno= new FineManoAvversario(nome, carteScoperte, stato, puntata);
+        i = i + 1;
+        Stato stato = Stato.valueOf(componenti[i]);
+        i = i + 1;
+        int puntata = Integer.valueOf(componenti[i]);
+        return ritorno = new FineManoAvversario(nome, carteScoperte, stato, puntata);
     }
 
-    private GiocatoreOnline giocatoreDaNome(String nome){
-        for(GiocatoreOnline giocatore :giocatori){
-            if(nome.equals(giocatore.getNome()))
+    private GiocatoreOnline giocatoreDaNome(String nome) {
+        for (GiocatoreOnline giocatore : giocatori) {
+            if (nome.equals(giocatore.getNome())) {
                 return giocatore;
+            }
         }
         return null;
     }
+
     public GiocatoreOnline getGiocatoreLocale() {
         return giocatoreDaNome(nomeLocale);
     }
@@ -244,21 +246,21 @@ public class PartitaOnlineController extends Observable implements ViewEventList
     }
 
     public GiocatoreOnline getMazziere() {
-        for(GiocatoreOnline gioc : giocatori){
-            if(gioc.isMazziere())
+        for (GiocatoreOnline gioc : giocatori) {
+            if (gioc.isMazziere()) {
                 return gioc;
+            }
         }
         return null;
     }
 
     private void setMazziere(String nome) {
-        for(GiocatoreOnline gioc : giocatori){
-            if(gioc.isMazziere())
+        for (GiocatoreOnline gioc : giocatori) {
+            if (gioc.isMazziere()) {
                 gioc.setMazziere(false);
+            }
         }
         giocatoreDaNome(nome).setMazziere(true);
     }
 
-    
-    
 }
