aa8344390e3a1870afce243bb66e5ec47aa5ccd7
marco <root@kali>
2017-06-28 12:10:29 +0200

    aggiunto sta puntando

---
+++ Sette_e_Mezzo_Server/src/partitaOnline/cambia/GiocatoreStaPuntando.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partitaOnline.cambia;
+
+/**
+ *
+ * @author root
+ */
+public class GiocatoreStaPuntando {
+    private String nome;
+
+    public GiocatoreStaPuntando(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "cambia\tGiocatoreStaPuntando\t" + nome ;
+    }
+    
+    
+    
+}
