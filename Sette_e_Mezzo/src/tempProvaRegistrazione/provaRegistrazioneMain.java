475cbfaa64883e8c9b5e08c2a160b493f479fe11
Martino <Martino@Martino-PC>
2017-06-28 10:49:07 +0200

    da fare: registrazineMenu che scambia info con server

--- Sette_e_Mezzo/src/tempProvaRegistrazione/provaRegistrazioneMain.java
+++
@@ -1,7 +0,0 @@
-package tempProvaRegistrazione;
-
-public class provaRegistrazioneMain {
-    public static void main(String[] args) throws InterruptedException {
-        new RegistrazioneMenu();
-    } 
-}
