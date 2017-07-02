6b32421294977ce314e01b61daf8665e5d8b735c
marco <root@kali>
2017-07-02 11:41:36 +0200

    aggiunti eventi

---
+++ Sette_e_Mezzo/src/partitaOnline/events/GiocatoreIniziaTurno.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partitaOnline.events;
+
+/**
+ *
+ * @author root
+ */
+public class GiocatoreIniziaTurno {
+    
+    private String giocatore;
+
+    public GiocatoreIniziaTurno(String giocatore) {
+        this.giocatore = giocatore;
+    }
+
+    public String getGiocatore() {
+        return giocatore;
+    }
+
+    @Override
+    public String toString() {
+        return "evento\tGiocatoreIniziaTurno\t" + giocatore;
+    }
+    
+    
+    
+}
