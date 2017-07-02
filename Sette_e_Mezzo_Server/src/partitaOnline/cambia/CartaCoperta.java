07bcd5440d3ce05de8f80020cc18c9ed99b8974c
marco <root@kali>
2017-07-02 12:50:48 +0200

    tolto valore mano

--- Sette_e_Mezzo_Server/src/partitaOnline/cambia/CartaCoperta.java
+++ Sette_e_Mezzo_Server/src/partitaOnline/cambia/CartaCoperta.java
@@ -14,17 +14,17 @@ import dominio.elementi_di_gioco.Carta;
 public class CartaCoperta {
     private String nome;
     private Carta coperta;
-    private double valore;
 
-    public CartaCoperta(String nome, Carta coperta, double valore) {
+
+    public CartaCoperta(String nome, Carta coperta) {
         this.nome = nome;
         this.coperta = coperta;
-        this.valore=valore;
+
     }
 
     @Override
     public String toString() {
-        return "cambia\tCartaCoperta\t" + nome + " " + coperta + " " + valore;
+        return "cambia\tCartaCoperta\t" + nome + " " + coperta ;
     }
     
     
