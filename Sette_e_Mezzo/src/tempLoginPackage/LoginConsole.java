ee2c3181b9991002162c92acacf358917bd3a4e9
root <root@kali>
2017-07-07 18:42:14 +0200

    tolto un sout

--- Sette_e_Mezzo/src/tempLoginPackage/LoginConsole.java
+++ Sette_e_Mezzo/src/tempLoginPackage/LoginConsole.java
@@ -75,10 +75,7 @@ public class LoginConsole {
         } catch (LoginEffettuato ex) {
             controller= new PartitaOnlineController(socketClient, in);
             new PartitaOnlineConsoleView(controller);
-//            while(true){
-//                
-//            }
-System.out.println("passato");
+
         }
 
     }
