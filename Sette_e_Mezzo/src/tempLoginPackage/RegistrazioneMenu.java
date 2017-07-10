a42f3df987fca435906dad1fb53abf14536221fa
marco <root@kali>
2017-07-10 14:11:39 +0200

    aggiunta possibilità errore se mail non esiste

--- Sette_e_Mezzo/src/tempLoginPackage/RegistrazioneMenu.java
+++ Sette_e_Mezzo/src/tempLoginPackage/RegistrazioneMenu.java
@@ -149,9 +149,11 @@ public class RegistrazioneMenu extends JFrame {
             if (risposta.equals("convalida inviata")) {
                 return true;
             } else if (risposta.equals("registrazione email gia esistente")) {
-                regFallita("email");
+                regFallita("email già esistente.");
             } else if (risposta.equals("registrazione username gia esistente")) {
-                regFallita("username");
+                regFallita("username già esistente.");
+            } else if (risposta.equals("registrazione email non valida")){
+                regFallita("email non valida.");
             }
 
         } catch (IOException ex) {
@@ -163,7 +165,7 @@ public class RegistrazioneMenu extends JFrame {
     private void regFallita(String motivo) {
         Font font = new Font("RegFallitaMsg", Font.BOLD, 60);
         messRegFallita = new JLabel("<html>Registrazione non riuscita,<br>"
-                + motivo + " già esistente."
+                + motivo 
                 + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Riprova per favore</html>");
         messRegFallita.setFont(font);
         messRegFallita.setForeground(Color.black);
