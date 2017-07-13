4826d35ade5a3a276a8c2c54662aaff43f670367
dbarattini <daniel.barattini@outlook.com>
2017-07-13 17:51:41 +0200

    Aggiunti javadoc di dominio.view

--- Sette_e_Mezzo/src/dominio/view/ViewEvent.java
+++ Sette_e_Mezzo/src/dominio/view/ViewEvent.java
@@ -6,11 +6,20 @@ import java.util.EventObject;
 public class ViewEvent extends EventObject{
     private Object arg;
     
+    /**
+     * Evento di una vista.
+     * 
+     * @param source Sorgente dell'evento
+     * @param arg Argomenti dell'evento
+     */
     public ViewEvent(Object source, Object arg) {
         super(source);
         this.arg = arg;
     }
 
+    /**
+     * @return Argomento dell'evento
+     */
     public Object getArg() {
         return arg;
     }
--- Sette_e_Mezzo/src/dominio/view/ViewEventListener.java
+++ Sette_e_Mezzo/src/dominio/view/ViewEventListener.java
@@ -3,6 +3,11 @@ package dominio.view;
 
 public interface ViewEventListener {
     
+    /**
+     * Gestisce il verificarsi di un evento nella vista.
+     * 
+     * @param evt evento generato nella vista
+     */
     public void ViewEventReceived(ViewEvent evt);
     
 }
