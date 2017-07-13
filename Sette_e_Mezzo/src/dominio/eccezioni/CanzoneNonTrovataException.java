1f922ad5222c1296aed04ef6a9017220a1060b17
samuele.colmi@gmail.com <samue@SamueleColmi>
2017-07-13 12:52:37 +0200

    javadoc #dominio.classi_dati.* javadoc #dominio.eccezzioni.canzonenontrovata

--- Sette_e_Mezzo/src/dominio/eccezioni/CanzoneNonTrovataException.java
+++ Sette_e_Mezzo/src/dominio/eccezioni/CanzoneNonTrovataException.java
@@ -4,10 +4,18 @@ package dominio.eccezioni;
 public class CanzoneNonTrovataException extends Exception {
     private String canzone;
 
+    /**
+     * 
+     * @param canzone canzone del gioco
+     */
     public CanzoneNonTrovataException(String canzone) {
         this.canzone = canzone;
     }
     
+    /**
+     * 
+     * @return la canzone del gioco
+     */
     public String getCanzone(){
         return canzone;
     }
