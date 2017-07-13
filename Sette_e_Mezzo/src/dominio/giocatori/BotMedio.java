12f6c9ce6d768f7352d65a58fee6efdcac3eaf99
samuele.colmi@gmail.com <samue@SamueleColmi>
2017-07-13 15:25:39 +0200

    javadoc #dominio.giocatori.botMedio javadoc #dominio.giocatori.botFacile

--- Sette_e_Mezzo/src/dominio/giocatori/BotMedio.java
+++ Sette_e_Mezzo/src/dominio/giocatori/BotMedio.java
@@ -4,15 +4,29 @@ import dominio.elementi_di_gioco.Mazzo;
 
 public class BotMedio extends Bot {
 
+    /**
+     * 
+     * @param nome nome del bot
+     * @param fiches fiches del bot
+     * @param mazzo mazzo della partita
+     */
     public BotMedio(String nome, int fiches, Mazzo mazzo) {
         super(nome, fiches, mazzo);
     }
 
+    /**
+     * 
+     * @return percentuale di sballare
+     */
     @Override
     protected int getPercentualePerPuntareTanto() {
         return 45;
     }
 
+    /**
+     * 
+     * @return percentuale di sballare
+     */
     @Override
     protected int getPercentualePerChiedereCarta() {
         return 40;
