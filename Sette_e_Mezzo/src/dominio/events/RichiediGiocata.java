14bf26f8313f20a67d14c4f6d0f103e2ef5d87b0
samuele.colmi@gmail.com <samue@SamueleColmi>
2017-07-13 15:02:02 +0200

    javadoc #dominio.eventi.richiedigiocata

--- Sette_e_Mezzo/src/dominio/events/RichiediGiocata.java
+++ Sette_e_Mezzo/src/dominio/events/RichiediGiocata.java
@@ -12,20 +12,38 @@ public class RichiediGiocata {
     ArrayList<Carta> carte_scoperte; 
     double valore_mano;
     
+    /**
+     * 
+     * @param carta_coperta carta coperta del giocatore
+     * @param carte_scoperte carte scoperte del giocatore
+     * @param valore_mano valore totale della mano del giocatore
+     */
     public RichiediGiocata(Carta carta_coperta, ArrayList<Carta> carte_scoperte, double valore_mano) {
         this.carta_coperta = carta_coperta;
         this.carte_scoperte = carte_scoperte;
         this.valore_mano = valore_mano;
     }
 
+    /**
+     * 
+     * @return carta coperta del giocatore
+     */
     public Carta getCartaCoperta() {
         return carta_coperta;
     }
 
+    /**
+     * 
+     * @return carte scoperte del giocatore
+     */
     public ArrayList<Carta> getCarteScoperte() {
         return carte_scoperte;
     }
 
+    /**
+     * 
+     * @return valore mano del giocatore
+     */
     public double getValoreMano() {
         return valore_mano;
     }
