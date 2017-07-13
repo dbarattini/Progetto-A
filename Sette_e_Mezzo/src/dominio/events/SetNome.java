68f30a910fa96c811dc9598096bc8ed8b43b9358
samuele.colmi@gmail.com <samue@SamueleColmi>
2017-07-13 15:07:31 +0200

    javadoc #dominio.eventi.setNome

--- Sette_e_Mezzo/src/dominio/events/SetNome.java
+++ Sette_e_Mezzo/src/dominio/events/SetNome.java
@@ -4,10 +4,18 @@ package dominio.events;
 public class SetNome {
     String nome;
 
+    /**
+     * 
+     * @param nome nome del giocatore
+     */
     public SetNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * 
+     * @return nome del giocatore
+     */
     public String getNome() {
         return nome;
     }
