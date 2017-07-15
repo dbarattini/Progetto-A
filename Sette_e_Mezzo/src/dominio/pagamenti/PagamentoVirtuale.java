639da52bd7ac23fac215e2ef19a3c62df179354c
samue <samue@SamueleColmi>
2017-07-15 15:31:27 +0200

    controllo javadoc #dominio.pagamenti

--- Sette_e_Mezzo/src/dominio/pagamenti/PagamentoVirtuale.java
+++ Sette_e_Mezzo/src/dominio/pagamenti/PagamentoVirtuale.java
@@ -5,6 +5,12 @@ import dominio.giocatori.Giocatore;
 
 public class PagamentoVirtuale extends PagamentoReale{
     
+    /**
+     * 
+     * @param pagante giocatore che paga
+     * @param pagato giocatore che viene pagato
+     * @param puntata quantità da pagare
+     */
     @Override
     protected void paga(Giocatore pagante, Giocatore pagato, int puntata) {
         if (pagante.isMazziere()) {
