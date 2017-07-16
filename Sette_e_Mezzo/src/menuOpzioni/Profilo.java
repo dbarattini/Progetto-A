ce30fe5156c643b5fed2666e927b1560a85f8a6e
marco <root@kali>
2017-07-16 10:02:16 +0200

    tolto il profilo (perchè si basava su sql che ho tolto

--- Sette_e_Mezzo/src/menuOpzioni/Profilo.java
+++ Sette_e_Mezzo/src/menuOpzioni/Profilo.java
@@ -1,6 +1,5 @@
 package menuOpzioni;
 
-import database.SQL;
 import static java.lang.System.in;
 import static java.lang.System.out;
 import java.util.Scanner;
@@ -11,38 +10,38 @@ import java.util.Scanner;
  */
 public class Profilo {
     
-    private SQL db;
+
     
 
     public Profilo() {
-         this.db = new SQL();
+         System.out.println("Il profilo non è ancora disponibile.");
     }
 
-    /**
-     * sceglie il profilo
-     */
-    public void selezionaProfilo() {
-        while(true) {
-            printOpzioniProfilo();
-            scegliProfilo();
-        }
-    }
-    
-    private void printOpzioniProfilo() {
-        out.println("\n");
-        out.println("Inserire nome dell'utente per visualizzare il profilo:\n");
-    }
-    
-    private void scegliProfilo() {
-        Scanner scanner = new Scanner(in);        
-        String input = scanner.next();
-        
-        if (db.esisteNome(input)) {
-            System.out.println("PROFILO:\n");
-            System.out.println("  -NOME: " +input);
-            System.out.println("  -FICHES: " +db.getFiches(input));
-            System.out.println("  -VITTORIE: " +db.getVittorie(input));
-        } else
-            System.out.println("Profilo non esistente\n");
-    }  
+//    /**
+//     * sceglie il profilo
+//     */
+//    public void selezionaProfilo() {
+//        while(true) {
+//            printOpzioniProfilo();
+//            scegliProfilo();
+//        }
+//    }
+//    
+//    private void printOpzioniProfilo() {
+//        out.println("\n");
+//        out.println("Inserire nome dell'utente per visualizzare il profilo:\n");
+//    }
+//    
+//    private void scegliProfilo() {
+//        Scanner scanner = new Scanner(in);        
+//        String input = scanner.next();
+//        
+//        if (db.esisteNome(input)) {
+//            System.out.println("PROFILO:\n");
+//            System.out.println("  -NOME: " +input);
+//            System.out.println("  -FICHES: " +db.getFiches(input));
+//            System.out.println("  -VITTORIE: " +db.getVittorie(input));
+//        } else
+//            System.out.println("Profilo non esistente\n");
+//    }  
 }
